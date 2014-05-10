1dae02e0cdfec2aa7f194dfa0cae5e904b50ceff
Adrian Padilla <adrian@adrianpadilla.net>
2014-05-09 23:40:37 -0700

    Hello world

---
+++ hello.hh
@@ -0,0 +1,11 @@
+<?hh
+
+$HowManyTimesIsAdrianAwesome = 100;
+
+for ($i = 1; $i <= $HowManyTimesIsAdrianAwesome; $i++) {
+    echo get_message( $i );
+}
+
+function get_message(int $index) : string {
+	return "Adrian is awesome ".$index." time \n";
+}
